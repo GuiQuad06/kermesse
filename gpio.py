29ed4936695f35166031c5ddd10f0567fac5647c
GuiQuad06 <guillaume.cuadrado@gmail.com>
2023-06-08 23:56:59 +0200

    Adding TKinter loop with a Count Down to Game over :)

--- gpio.py
+++ gpio.py
@@ -17,16 +17,6 @@ from playsound import playsound
 GPIO.setwarnings(False)
 playsound('music/background.mp3', block=False)
 
-# Event : Eteindre LED verte / Mettre la LED rouge pendant 2 sec, son "tu as perdu"
-def contact_callback(channel):
-    GPIO.remove_event_detect(channel)
-    #print("Event s'est produit sur" + str(channel) )
-    GPIO.output(18, GPIO.HIGH)
-    GPIO.output(23, GPIO.LOW)
-    playsound('music/gameover.wav')
-    #time.sleep(2)
-    Gpio()
-
 class Gpio:
     def __init__(self):
         #PIN MAPPING FOR THE RASPBERRY
@@ -40,5 +30,4 @@ class Gpio:
         GPIO.setup(self.contact, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.red_led, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.green_led, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.add_event_detect(self.contact, GPIO.FALLING, callback=contact_callback)
     
--- kermesse.py
+++ kermesse.py
@@ -11,21 +11,57 @@
 
 #IMPORTS
 from gpio import *
-#import pymedia
 import time
+import tkinter as tk
+from tkinter import *
+from threading import Thread
+
+Gpio()
+
+class check_contact(Thread):
+
+    def __init__(self, labelText):
+        Thread.__init__(self)
+        self.labelText = labelText
+        self.tries = 5
+
+    def checkloop(self):
+        while True:
+            if GPIO.input(4) == 1:
+                if self.tries > 0:
+                    self.labelText.set(self.tries)
+                    print("aouch !")
+                    self.tries = self.tries - 1
+                else:
+                    self.labelText.set("GAME OVER")
+                    print("game over")
+                    self.tries = 5
+                time.sleep(0.5)
+                while GPIO.input(4) == 1: pass
+
+#Création de l'objet Tkinter
+window = Tk()
+
+label_text = StringVar()
+display = Label(window, textvariable=label_text) 
+display.config(font=('Helvetica', 100, 'bold'))
+display.pack(fill = BOTH, expand = True)
+window.title("LABYRINTHE 3000") 
+
+#Définition du thread qui poll l'action du contact
+contact_handler = check_contact(label_text)
+contact = Thread(target=contact_handler.checkloop)
+contact.start()
+
+#Ouverture de la fenêtre avant la boucle du jeu
+w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+window.geometry("%dx%d" % (w, h))
+open
+window.mainloop()
+
 
 def main():
     pass
-    # Init : configure les gpios, met la musique de fond, allume la LED
-    Gpio()
-    
-    #player = pymedia.Player()
-    #player.start()
-    #player.startPlayback('toto.mp3')
-
-    while True:
-        # Boucle infinie : Attends un event + sleep
-        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
